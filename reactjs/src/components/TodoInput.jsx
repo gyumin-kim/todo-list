44efb7a46ae770a61159c3efd43d004eeb0bbe33
Gyumin Kim <starkying@gmail.com>
2019-03-17 17:42:28 +0900

    Developing adding todoitem functionality

---
+++ reactjs/src/components/TodoInput.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class TodoInput extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.createNewTodo.bind(this)}>
+          <input type="text" 
+                  ref={ref => {
+                    this._inputElement = ref
+                  }} />
+          <button type="submit">추가</button>
+        </form>
+      </div>
+    );
+  }
+
+  createNewTodo(e) {
+    let title = this._inputElement.value;
+    
+    if (title === "") {
+      alert("내용을 입력하세요")
+      e.preventDefault();
+      return;
+    }
+
+    fetch("/api/item", {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+      })
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+};
+
+export default TodoInput;
